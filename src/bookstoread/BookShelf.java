80119065397888a1aa29ac2dee04c24d3e8130f1
Christ Uriel <achristuriel@gmail.com>
2026-05-06 11:10:59 +0200

    Modification methode arrange et ajout d'un nouveau test reussi

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,5 +1,6 @@
 package bookstoread;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BookShelf {
         private final List<String> books = new ArrayList<>();
@@ -11,8 +12,8 @@ public class BookShelf {
     }
 
     public List<String> arrange() {
-        books.sort(Comparator.naturalOrder());
-        return books;
+        return books.stream().sorted().collect(Collectors.toList());
     }
+
 
 }
