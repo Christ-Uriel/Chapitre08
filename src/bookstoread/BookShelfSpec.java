85c82cff079f1318abe947b040fe77edf606fac8
Christ Uriel <achristuriel@gmail.com>
2026-05-07 15:20:13 +0200

    Ajout de @DisplayName pour organisation hiérarchique des tests sans limite de niveaux d'imbrication.

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -11,6 +11,7 @@ import java.util.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class BookShelfSpec {
@@ -111,5 +112,37 @@ public class BookShelfSpec {
         assertThat(booksByPublicationYear).containsKey(Year.of(2008)).containsValues(Arrays.asList(effectiveJava, cleanCode));
         assertThat(booksByPublicationYear).containsKey(Year.of(2004)).containsValues(Collections.singletonList(codeComplete));
         assertThat(booksByPublicationYear).containsKey(Year.of(1975)).containsValues(Collections.singletonList(mythicalManMonth));
+    }
+    @Nested
+    @DisplayName("Est vide")
+    class IsEmpty {
+        @Test
+        @DisplayName("Quand aucun livre n'y est ajouté")
+        public void emptyBookShelfWhenNoBookAdded() {
+            List<Book> books = shelf.books();
+            assertTrue(books.isEmpty(), () -> "BookShelf devrait être vide.");
+        }
+        @Test
+        @DisplayName("Quand add est appelé sans livres")
+        void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+            shelf.add();
+            List<Book> books = shelf.books();
+            assertTrue(books.isEmpty(), () -> "BookShelf devrait être vide.");
+        }
+    }
+
+    @Nested
+    @DisplayName("Après avoir ajouté des livres")
+    class BooksAreAdded {
+        @Test
+        @DisplayName("Contient deux livres")
+        void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+            // Test case removed for brevity
+        }
+        @Test
+        @DisplayName("Renvoie au client une collection de livres immuable ")
+        void bookshelfIsImmutableForClient() {
+            // Test case removed for brevity
+        }
     }
 }
